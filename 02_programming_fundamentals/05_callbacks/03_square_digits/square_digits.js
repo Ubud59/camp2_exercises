3be02827491a28d4807ff2865afe7a72c99c946a
Romuald Falce <romuald.falce@decathlon.com>
2017-11-27 16:41:27 +0100

    squareDigits

--- 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
+++ 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
@@ -8,7 +8,21 @@
 // Note: The function accepts an integer and returns an integer
 
 function squareDigits(number) {
-  // Your code here
+  let nombre = number.toString();
+  let nb = 0;
+  let nb_square = 0;
+  let chaine_nb_square = "";
+  // console.log("nombre :" + nombre);
+  for (let i=0; i < nombre.length; i++) {
+    nb = parseInt(nombre[i], 10);
+    // console.log("nb :" + nb);
+    nb_square=nb*nb;
+    // console.log("nb_square :" + nb_square);
+    chaine_nb_square = chaine_nb_square + nb_square.toString();
+  }
+  // console.log("chaine_nb_square :" + chaine_nb_square);
+  // console.log("nb_square :" + parseInt(chaine_nb_square,10));
+  return parseInt(chaine_nb_square, 10);
 }
 
 // Do not remove last lines, it is for tests
