e5380883372033c64701471b3a77d52580705613
Romuald Falce <romuald.falce@decathlon.com>
2017-11-27 15:48:16 +0100

    fjfjfj

--- 02_programming_fundamentals/05_callbacks/02_create_callbacks/create_callback.js
+++ 02_programming_fundamentals/05_callbacks/02_create_callbacks/create_callback.js
@@ -2,8 +2,10 @@ const log = (text) => console.log(text);
 
 // A function that takes two parameters, this first one being an object like below
 // and the second one being a callback function.
-const getInput = (____, ____) => {
+
+const getInput = (param, fct) => {
   // When finished, we want to log the key params...
+  fct(param.key);
 };
 
 getInput({user: "Lorem", key: "arrow_up"}, log);
