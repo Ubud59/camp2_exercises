9fe9105db4d6f6dca5f1c70310ba19a86cbfbb31
Romuald Falce <romuald.falce@decathlon.com>
2017-11-30 12:16:04 +0100

    chaining

--- 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
+++ 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
@@ -1,3 +1,29 @@
 // See Sparta courses for the exercise summary
 
+const car = {
+  speed: 0,
+  km_total: 0,
+
+  start: function() { // will reset all the car data
+    this.speed = 0;
+    this.km_total = 0;
+    return this;
+  },
+
+  changeSpeed(speed) { // will change the km/h speed of the car
+    this.speed = speed;
+    return this;
+  },
+
+  drive(minutes) { // will make your car drive at the previously set speed for that amount of time in minutes
+    this.km_total = this.km_total + (this.speed/60) * minutes;
+    return this;
+  },
+
+  showDistance() { // will print the distance you drove as the number of kilometers
+    return console.log(`${this.km_total} Km`);
+  }
+}
+
+
 module.exports = car;
