d1ea0812adde767451442cc99d2612e7b033efdb
Romuald Falce <romuald.falce@decathlon.com>
2017-11-30 13:50:20 +0100

    coffee machine

--- 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
+++ 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
@@ -2,6 +2,33 @@
 
 // Coffee Machine usage. Insert your code above this comment
 
+const machine = {
+  litersOfCoffee: 0,
+
+  expresso: function() {
+    if (this.litersOfCoffee < 0.08) {
+      return false;
+    } else {
+      this.litersOfCoffee -= 0.08;
+      return true;
+    }
+  },
+
+  longCoffee: function() {
+    if (this.litersOfCoffee < 0.15) {
+      return false;
+    } else {
+      this.litersOfCoffee -= 0.15;
+      return true;
+    }
+  },
+
+  fillWithLitersOfCoffee: function(nb_liters) {
+    return this.litersOfCoffee += nb_liters;
+  }
+
+}
+
 machine.fillWithLitersOfCoffee(10);
 console.log(machine.expresso()) // => true
 console.log(machine.litersOfCoffee) // => 9.92
